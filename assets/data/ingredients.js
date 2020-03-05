f673ab9e8f53ba746efe2498caa4eb3fda9771e9
Wilson Lam <lin-1987@live.com>
2020-03-04 21:52:32 -0800

    changed some data and dynamically generated cols and row

--- assets/data/ingredients.js
+++ assets/data/ingredients.js
@@ -1,14 +1,198 @@
 const ingredients = [
     {
+        id: "name1",
         name: "Apple",
         description: "Antioxidant\n Cardiovascular Benefits\n Blood Sugar Regulation",
         link: "https://www.medicalnewstoday.com/articles/267290.php#benefits",
-        img: "./assets/img/ingredients/apple.jpg"
+        img: "./assets/img/ingredients/apple.jpg",
+        style: "border: 5px solid rgb(186, 17, 11);"
     },
     {
+        id: "name2",
         name: "Avocado",
         description: "Anti-Inflammatory\n Promote Heart Healthy\n Anti-Cancer Benefits",
         link: "https://www.medicalnewstoday.com/articles/318620.php#nutrition",
         img: "./assets/img/ingredients/avocado.jpg"
+    },
+    {
+        id: "name3",
+        name: "Banana",
+        description:  "Protect Eyesight\n Prevent High Blood Pressure\n Best Source of Potassium",
+        link: "https://www.medicalnewstoday.com/articles/271157.php#nutrition",
+        img: "./assets/img/ingredients/banana.jpg"
+    },
+    {
+        id: "name4",
+        name: "Beets",
+        description: "Antioxidants\n Detoxification\n Anti-Inflammatory",
+        link: "https://www.medicalnewstoday.com/articles/277432.php",
+        img: "./assets/img/ingredients/beet.jpg"
+    },
+    {
+        id: "name5",
+        name: "Blackberry",
+        description: "Vitamin E\n Antioxidants\n Vitamin C",
+        link: "https://www.healthline.com/nutrition/11-reasons-to-eat-berries#section1",
+        img: "./assets/img/ingredients/blackberry.jpg"
+    },
+    {
+        id: "name6",
+        name: "Blueberry",
+        description: "Cardiovascular Benefits\n Anti-Cancer Benefits\n Supports Vision Health",
+        link: "https://www.medicalnewstoday.com/articles/287710.php#benefits",
+        img: "./assets/img/ingredients/blueberry.jpg"
+    },
+    {
+        id: "name7",
+        name: "Cantaloupe",
+        description: "Anti-Inflammatory\n Antioxidants\n Vitamin A",
+        link: "https://www.medicalnewstoday.com/articles/279176.php",
+        img: "./assets/img/ingredients/cantaloupe.jpg"
+    },
+    {
+        id: "name8",
+        name: "Carrot",
+        description: "Vision Health\n Antioxidants\n Anti-Cancer Benefits",
+        link: "https://www.medicalnewstoday.com/articles/270191.php",
+        img: "./assets/img/ingredients/carrot.jpg"
+    },
+    {
+        id: "name9",
+        name: "Celery",
+        description: "Anti-Inflammatory\n Digestive Support\n Cardiovascular Support",
+        link: "https://www.medicalnewstoday.com/articles/270678.php#benefits",
+        img: "./assets/img/ingredients/celery.jpg"
     }
 ];
+
+const ingredients2 = [
+    {
+        id: "name10",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name11",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name12",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name13",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name14",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name15",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name16",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name17",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name18",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+];
+
+const ingredients3 = [
+    {
+        id: "name19",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name20",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name21",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name22",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name23",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name24",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name25",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name26",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+    {
+        id: "name27",
+        name: "",
+        description: "",
+        link: "",
+        img: ""
+    },
+]
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,37 @@
 // Ingredients Page Results Generate Display
 $(document).ready(() => {
-  console.log(ingredients);
+  const ulEl = $("#listContainer");
+  ingredients.map(ingredient => {
+    const $li = $("<li>");
+    const $img = $("<img>");
+    const $row = $("<row>");
+    const $col1 = $("<col>");
+    const $col2 = $("<col>");
+    const $col3 = $("<col>");
+    const $col4 = $("<col>");
+    const $a = $("<a>");
+
+    // Image dynamic creation
+    $img.addClass("fruitsCollection")
+    $img.attr("src", ingredient.img)
+    // Col1 Dynamic Creation
+    $col1.addClass("col-sm-2 mx-0");
+    // Col2 Dynamic Creation
+    $col2.addClass("col-sm-3 fruitsText align-self-center colBottom")
+    $col2.attr("id", ingredient.id);
+    // Row Dynamic Creation
+    $row.addClass("row fruitRow")
+    $row.attr("id", ingredient.name);
+
+    // Appending
+    $col1.append($img);
+    $row.append($col1);
+    $li.append($row);
+    ulEl.append($li);
+    })
+    
+
+  
   
 })
 
