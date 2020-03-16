1f92eebf124ec35c87d5a70121666af6d919d12e
Wilson Lam <lin-1987@live.com>
2020-03-16 00:50:10 -0700

    Input data + pictures for resources page

---
+++ assets/data/resources.js
@@ -0,0 +1,47 @@
+const resourceCards1 = [
+    {
+        title: "BLENDING VS JUICING",
+        p1: "Which is better: juicing or blending? Does one offer more health benefits than the other?",
+        img: "./assets/img/resources/green_Juice.jpg",
+        link: "https://www.foodmatters.com/articles-1/juicing-vs-blending-which-one-is-better"
+    },
+    {
+        title: "WHY WE OFFER HONEY, NOT SUGAR!",
+        p1: "What are their differences? Is hoey better than sugar?",
+        img: "./assets/img/resources/honey.jpg",
+        link: "https://www.benefits-of-honey.com/honey-vs-sugar.html"
+    },
+    {
+        title: "IS IT BETTER TO GET VITAMINS FROM FOOD OR SUPPLEMENTS?",
+        p1: "Are natural vitamins better than synthetic vitamins?",
+        img: "./assets/img/resources/pills.png",
+        link: "https://www.health.harvard.edu/newsweek/Listing_of_vitamins.htm"
+    },
+];
+
+const resourceCards2 = [
+    {
+        title: "CALIFORNIA AND FLORIDA AVOCADOS DIFFERENCE",
+        p1: "Blend It Healthy uses California avocados which contain less calories.",
+        img: "./assets/img/resources/avocado.jpg",
+        link: "https://www.quickanddirtytips.com/health-fitness/healthy-eating/what-s-the-difference-between-california-and-florida-avocados"
+    },
+    {
+        title: "BEST DETOX FRUIT WATERS",
+        p1: "A delicious way to cleanse your system.",
+        img: "./assets/img/resources/lemons.jpg",
+        link: "https://www.healthline.com/nutrition/detox-water-101"
+    },
+    {
+        title: "HERE'S WHY WE USE WHEY PROTEIN ISOLATE!",
+        p1: "It is a high-quality protein replete with all essential amino acids.",
+        img: "./assets/img/resources/vanilla.jpg",
+        link: "https://www.myhealthylivingcoach.com/what-is-whey-protein-isolate-and-how-can-it-help-you"
+    },
+    {
+        title: "WHAT IS HOLISTIC MEDICINE?",
+        p1: "A person's mind and spirit are treated as important to health as the body.",
+        img: "./assets/img/resources/Mindfulness.png",
+        link: "https://www.webmd.com/balance/guide/what-is-holistic-medicine"
+    },
+]
