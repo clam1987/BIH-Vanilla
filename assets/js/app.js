1c977c67c13a3e4a59469af910f95c04042e7e7a
Wilson Lam <48064335+clam1987@users.noreply.github.com>
2021-02-27 20:07:19 -0800

    updated new banner

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,32 @@
 // Submit Button
-$(".subBtn").on("click", function() {
-    const footerInputVal = $(".footerInput").val();
-    if (validateEmail(footerInputVal)) {
-        $(".footerInput").hide();
-        $(".subBtn").hide();
-        console.log("Thank you for signing up")
-        $(".col-md-6").append($(`<h3 style="text-align: center;">Thank you for subscribing</h3>`))
-        // In the future, this will be logic to log into the database
-    } else {
-        console.log("you must enter a valid email")
-    }
-});
+$(document).ready(function() {
+    $(".subBtn").on("click", function() {
+        const footerInputVal = $(".footerInput").val();
+        if (validateEmail(footerInputVal)) {
+            $(".footerInput").hide();
+            $(".subBtn").hide();
+            console.log("Thank you for signing up")
+            $(".col-md-6").append($(`<h3 style="text-align: center;">Thank you for subscribing</h3>`))
+            // In the future, this will be logic to log into the database
+        } else {
+            console.log("you must enter a valid email")
+        }
+    });
 
-// Validation Function
-const validateEmail = email => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  };
+    $("#disclaimer-modal").modal('show');
+    // console.log($("#disclaimer-modal"))
+    
+    // Validation Function
+    const validateEmail = email => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      };
+
+    $("#disc-close").on("click", function() {
+        // $("#disclaimer-modal").removeClass('show');
+        // $("#disclaimer-modal").removeClass('show');
+        // $("#disclaimer-modal").addClass('hide');
+        $("#disclaimer-modal").modal('hide')
+        console.log($("#disclaimer-modal"))
+    });
+});
